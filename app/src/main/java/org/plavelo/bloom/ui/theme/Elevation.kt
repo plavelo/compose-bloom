8081118891b9617081b1f0d277f24c3f4bfa9453
plavelo <plastic.velocity@gmail.com>
2021-03-13 10:54:04 +0900

    added shapes and elevations

--- app/src/main/java/org/plavelo/bloom/ui/theme/Elevation.kt
+++ app/src/main/java/org/plavelo/bloom/ui/theme/Elevation.kt
@@ -15,11 +15,10 @@
  */
 package org.plavelo.bloom.ui.theme
 
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
-val shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(24.dp),
-)
+object Elevations {
+    val elevation01 = 1.dp
+    val elevation02 = 2.dp
+    val elevation16 = 16.dp
+}
